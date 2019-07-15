e37c3d9a30fba3c3e359e93e31bc5850f420c368
daniel diaz <danieldiazg01@gmail.com>
2019-07-15 00:47:11 -0400

    import omp, define number of theads

--- main.cpp
+++ main.cpp
@@ -2,18 +2,22 @@
 #include <iostream>
 #include <vector>
 #include <complex>
+#include "omp.h"
 #include "qregister.hpp"
+
+#define NUM_THREADS 4
 
 using namespace std;
 
 int main() {
+  omp_set_num_threads(NUM_THREADS);
   QRegister qreg({{0,1}, {1,0}, {1/sqrt(2), 1/sqrt(2)}, {1/sqrt(2), -1/sqrt(2)}, {1/sqrt(2), {0,1/sqrt(2)}}});
   vector <complex<double> > result;
   result = qreg.getStateVector();
   qreg.applyQFT();
 
   result = qreg.getStateVector();
-  cout<<"Final states: "<<endl;
+  cout<<"n_qbits: "<< qreg.getNumberQubits() <<endl;
   for (int i = 0; i < result.size(); i++)
     cout<<"result["<<i<<"] = "<<result[i]<<endl;
   
